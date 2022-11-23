99895f163d9961fc5acaf4022e0581fd0551510d
Luis Martínez Sánchez de Lara <94903069+LuisMart7@users.noreply.github.com>
2022-11-23 09:46:56 +0100

    Avanza en la tarea https://github.com/danielsp13/SuperCatch/issues/11 completando pregunta.py

--- src/pregunta.py
+++ src/pregunta.py
@@ -1,1 +1,33 @@
+from dataclasses import dataclass
+
+@dataclass
 class Pregunta:
+
+    numero: int
+    respuesta: str
+    palabras_clave: list
+
+    def __init__(self, num: int):
+        self.numero = num
+        self.respuesta = ''
+        self.palabras_clave = []
+
+    def __init__(self, num: int, res: str):
+        self.numero = num
+        self.respuesta = res
+        self.palabras_clave = []
+    
+    def get_numero(self):
+        return self.numero
+
+    def get_respuesta(self):
+        return self.respuesta
+
+    def get_palabra_clave(self, posicion: int):
+        return self.palabras_clave[posicion]
+    
+    def get_posicion_palabra(self, palabra: str):
+        return self.palabras_clave.index(palabra)
+
+    def aniadir_palabra_clave(self, palabra: str):
+        self.palabras_clave.append(palabra)
