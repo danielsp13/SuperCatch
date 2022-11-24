2dfc9ced3cbfdc2a97a806890c61601934ce0a00
Luis Martínez Sánchez de Lara <94903069+LuisMart7@users.noreply.github.com>
2022-11-24 15:14:31 +0100

    Avanza en la tarea https://github.com/danielsp13/SuperCatch/issues/15 completando la clase Alumno

--- src/alumno.py
+++ src/alumno.py
@@ -1,1 +1,26 @@
 class Alumno:
+
+    dni: int
+    nombre: str
+    apellidos: str
+    calificaciones: list
+
+    def __init__(self, id:int):
+        self.dni = id
+        self.nombre = ''
+        self.apellidos = ''
+        self.calificaciones = []
+
+    def get_dni(self):
+        return self.dni
+
+    def get_nombre(self):
+        return self.nombre
+    
+    def get_apellidos(self):
+        return self.apellidos
+
+    def aniadir_nota(self, nota):
+        self.calificaciones.append(nota)
+    
+    
