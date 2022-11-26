481e30febc42a2161717d2cb9cf4cdad87451c0c
Luis Martínez Sánchez de Lara <94903069+LuisMart7@users.noreply.github.com>
2022-11-26 12:11:45 +0100

    Feat(alumno.py): Add frozen dataclass and remove getters
    Según lo hablado en https://github.com/danielsp13/SuperCatch/pull/10#discussion_r1027843960 y https://github.com/danielsp13/SuperCatch/pull/10#discussion_r1032136841 hemos cambiado la naturaleza de la clase a Dataclass con frozen (inmutable) y hemos eliminado las funciones get

--- src/alumno.py
+++ src/alumno.py
@@ -1,26 +1,19 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
 class Alumno:
 
+    '''Clase que representa un alumno que realiza el examen'''
     dni: int
     nombre: str
     apellidos: str
-    calificaciones: list
+    curso: int
 
-    def __init__(self, id:int):
+    def __init__(self, id: int):
         self.dni = id
         self.nombre = ''
         self.apellidos = ''
-        self.calificaciones = []
-
-    def get_dni(self):
-        return self.dni
-
-    def get_nombre(self):
-        return self.nombre
+        self.curso = 0
     
-    def get_apellidos(self):
-        return self.apellidos
-
-    def aniadir_nota(self, nota:int):
-        self.calificaciones.append(nota)
     
     
