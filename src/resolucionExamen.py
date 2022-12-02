67cc4d9b3685887febb7434c69d1eb55a6e3d05b
Luis Martínez Sánchez de Lara <94903069+LuisMart7@users.noreply.github.com>
2022-12-02 15:12:32 +0100

    Fix deleting inecessary set methods and name field
    Según la https://github.com/danielsp13/SuperCatch/blob/main/docs/user-stories.md#black_nib-hu01-dolores-quiere-agilizar-la-tarea-de-correcci%C3%B3n-de-ex%C3%A1menes y https://github.com/danielsp13/SuperCatch/blob/main/docs/user-stories.md#black_nib-hu03-irene-necesita-entrenar-para-sus-ex%C3%A1menes, no es necesario crear métodos que añadan preguntas y respuestas además de la descripción del examen que se realiza pues no se tendrá constancia de un registro de exámenes.

--- src/resolucionExamen.py
+++ src/resolucionExamen.py
@@ -7,29 +7,19 @@ from dataclasses import dataclass
 class resolucionExamen:
 
     '''Clase que representa un examen y su corrección como:
-        - Tema asociado al examen como cadena (ej: Tema 4)
         - Tipo de examen: prueba o real
         - Secuencia de preguntas
         - Secuencia de respuestas de todos los alumnos'''
-    tema: str
     tipo = ['PRUEBA', 'REAL']
     preguntas: list
     respuestas: list
 
-    def __init__(self, tema_examen: str):
-        self.tema = tema_examen
+    def __init__(self):
         self.tipo = 0
         self.preguntas = []
         self.respuestas = []
 
-    def __init__(self, tema_examen: str, type: str):
-        self.tema = tema_examen
+    def __init__(self, type: str):
         self.tipo = type
         self.preguntas = []
         self.respuestas = []
-
-    def aniadir_pregunta(self, preg: Pregunta):
-        self.preguntas.insert(preg.numero-1, preg)
-
-    def aniadir_respuesta(self, resp: Respuesta):
-        self.respuestas.append(resp)
