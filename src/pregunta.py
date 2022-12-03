a048a3e2fc20a629dd69b8cf9130b637e1996e43
Luis Martínez Sánchez de Lara <94903069+LuisMart7@users.noreply.github.com>
2022-12-03 12:46:01 +0100

    Fix init methods
    Se ha arreglado la asignación de campos en la inicialización debido a que se usa instancias del tipo Frozen.

--- src/pregunta.py
+++ src/pregunta.py
@@ -7,16 +7,16 @@ class Pregunta:
     numero: int
     respuesta: str
     palabras_clave: list
-
+        
     def __init__(self, num: int):
-        self.numero = num
-        self.respuesta = ''
-        self.palabras_clave = []
+        object.__setattr__(self, "numero", num)
+        object.__setattr__(self, "respuesta", '')
+        object.__setattr__(self, "palabras_clave", [])
 
     def __init__(self, num: int, res: str):
-        self.numero = num
-        self.respuesta = res
-        self.palabras_clave = []
+        object.__setattr__(self, "numero", num)
+        object.__setattr__(self, "respuesta", res)
+        object.__setattr__(self, "palabras_clave", [])
 
     def get_palabra_clave(self, posicion: int):
         return self.palabras_clave[posicion]
