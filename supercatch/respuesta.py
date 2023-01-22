e76256e6e70e22d6014be5e3e65eaa0c7b205aa4
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2023-01-22 14:55:03 +0100

    :bug: corregida inicialización, #33
    ahora si es frozen

--- supercatch/respuesta.py
+++ supercatch/respuesta.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Respuesta:
-    texto: str
+	texto: str
 
-    def __init__(self,texto):
-        self.texto = texto
+	def __init__(self,texto):
+		object.__setattr__(self,'texto',texto)
