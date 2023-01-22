7e4f60c3d3b78cf161b4cf185dc2158c2cc944f8
Daniel Pérez Ruiz <danielperezruiz.10@gmail.com>
2023-01-22 23:06:43 +0100

    eliminación de símbolos en lista de tokens, #44
    esta implementación pasa todos los tests. Es necesario eliminar algunos símbolos explícitamente para verificar casos de excepción

--- supercatch/tokensRespuesta.py
+++ supercatch/tokensRespuesta.py
@@ -1,5 +1,6 @@
 from supercatch.respuesta import Respuesta
 
+from string import punctuation
 from nltk.tokenize import word_tokenize
 
 class TokensRespuesta:
@@ -18,10 +19,27 @@ class TokensRespuesta:
 		return self.Tokens
 		
 	def tokenizarTexto(self):
-		tokens = word_tokenize(self.Respuesta.texto.replace('¿','').replace('¡',''))
+		unhandled_symbols = ['.','¡','¿']
+		tokensFormateados = self.Respuesta.texto.replace('·','')
+		for symbol in unhandled_symbols:
+			tokensFormateados = tokensFormateados.replace(symbol,'')
+		
+		tokens = word_tokenize(tokensFormateados)
 		
 		if len(tokens) > 0:
 			self.Tokens = tokens
 			
 		else:
 			raise Exception("No hay contenido en la respuesta proporcionada.")
+			
+	
+	def eliminarSimbolos(self):
+		tokens = [tk for tk in self.Tokens if tk not in punctuation]
+		
+		if len(tokens) > 0:
+			self.Tokens = tokens
+			
+		else:
+			raise Exception("El texto no contiene palabras.")
+
+	
